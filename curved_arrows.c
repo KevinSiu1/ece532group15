4776afbac35e5fa0e204d4c6b1cee805da5f0c10
Yinghui (Vivien) Fan <yinghui.fan@mail.utoronto.ca>
2017-03-21 11:52:40 -0400

    Add files via upload

---
+++ curved_arrows.c
@@ -0,0 +1,51 @@
+// curved arrows
+u32 TextData2[64];
+
+// (vertical) up-right arrow
+TextData2[0]  = 0x190d073f;  // top half
+TextData2[1]  = 0x40602031;
+TextData2[2]  = 0x30206040; // bottom half
+TextData2[3]  = 0x02060c18;	
+
+// (vertical) up-left arrow
+TextData2[4]  = 0x98b0e0fc; // top half
+TextData2[5]  = 0x0206048c; 
+TextData2[6]  = 0x0c040602; // bottom half
+TextData2[7]  = 0x40603018; 
+
+// (vertical) down-right arrow
+TextData2[8]  = 0x180c0602; // top half
+TextData2[9]  = 0x40602030; 
+TextData2[10] = 0x31206040; // bottom half
+TextData2[11] = 0x3f070d19; 
+
+// (vertical) down-left arrow
+TextData2[12] = 0x18306040; // top half
+TextData2[13] = 0x0206040c; 
+TextData2[14] = 0x80060602; // bottom half
+TextData2[15] = 0xfce0b098;  
+
+// (horizontal) up-right arrow 
+TextData2[16] = 0x180e0300; // left half
+TextData2[17] = 0x00c06030; 
+TextData2[18] = 0x1971c000; // right half
+TextData2[19] = 0x1f03070d;
+
+// (horizontal) up-left arrow
+TextData2[20] = 0x988e0300; // left half
+TextData2[21] = 0xb0e0c0f8;
+TextData2[22] = 0x1870c000; // rigth half
+TextData2[23] = 0x0003060c;
+
+// (horizontal) down-right arrow
+TextData2[24] = 0x3060c000; // left half
+TextData2[25] = 0x00030e18;
+TextData2[26] = 0x0d07031f; // right half
+TextData2[27] = 0x00c07119;
+
+// (horizontal) down-left arrow
+TextData2[28] = 0xb0e0c0f1; // left half
+TextData2[29] = 0x00038e98;
+TextData2[30] = 0x0c060300; // right half
+TextData2[31] = 0x00c07018;
+
